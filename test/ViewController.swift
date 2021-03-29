3ed9c86427ee3240ab997c33cd2655d206d91194
Kahina Lounis <kahina.lounis@europcar.com>
2021-03-29 16:00:52 +0200

    code refactoring

--- test/ViewController.swift
+++ test/ViewController.swift
@@ -16,23 +16,40 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-      
     }
-    func calculateRate( userValue : Double) -> Double {
-       if userValue < 1 {
-           return userValue * 10
-       }
-       else {
-           return userValue / 10
-       }
+    
+    func calculateRate20or10(userValue : Double, rate : Double) -> Double {
+        
+        if userValue < 1 {
+            return userValue * rate
+        }
+        else {
+            return userValue / rate
+        }
+    }
+    
+    func displayAlert() {
+        let alert = UIAlertController(title: "Error", message: "Please enter a number", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Dismiss", style: .default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
     }
 
-    @IBAction func calculate(_ sender: UIButton) {
+    @IBAction func calculate10(_ sender: UIButton) {
         if let numberTextFieldText = numberTextField.text, let numberToCalculate = Double(numberTextFieldText) {
-             
-               resultLabel.text = String(calculateRate( userValue : numberToCalculate))
+             resultLabel.text = String(calculateRate20or10( userValue : numberToCalculate, rate : 10))
             }
+        else {
+            displayAlert()
         }
+    }
     
+    @IBAction func calculate20(_ sender: UIButton) {
+        if let numberTextFieldText = numberTextField.text, let numberToCalculate = Double(numberTextFieldText) {
+               resultLabel.text = String(calculateRate20or10(userValue: numberToCalculate, rate: 20))
+            }
+        else {
+            displayAlert()
+        }
+    }
 }
 
--- testTests/calculatingTests.swift
+++ testTests/calculatingTests.swift
@@ -11,15 +11,17 @@ import XCTest
 
 class calculatingTests : XCTestCase {
     
-    func testCalculatingRate(){
-        
+    func testCalculatingRate() {
         let vc = ViewController()
-        let deviser10 = vc.calculateRate(userValue: 100)
+        let deviser10 = vc.calculateRate20or10(userValue: 100, rate : 10)
         XCTAssertEqual(10, deviser10)
-        let multiply10 = vc.calculateRate(userValue: -7)
+        let multiply10 = vc.calculateRate20or10(userValue: -7, rate : 10)
         XCTAssertEqual(-70, multiply10)
+        let deviser20 = vc.calculateRate20or10(userValue: 100, rate : 20)
+        XCTAssertEqual(5, deviser20)
+        let multiply20 = vc.calculateRate20or10(userValue: -7, rate : 20)
+        XCTAssertEqual(-140, multiply20)
     }
-   
-
 }
+
 
