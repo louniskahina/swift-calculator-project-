f92cdaf395a359fb7815d33cd3dcec4d1e2b44a1
Kahina Lounis <kahina.lounis@europcar.com>
2021-04-15 23:53:39 +0200

    add unit tests on math functionality + add second component to notes functionality

--- test/ViewController.swift
+++ test/ViewController.swift
@@ -8,24 +8,44 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
     @IBOutlet weak var resultLabel: UILabel!
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        resultLabel.text = ""
+    
+    @IBAction func clearButton(_ sender: UIButton) {
+        resultLabel.text! = ""
+    }
+    
+    @IBAction func resultButton(_ sender: UIButton) {
+        if let formule = resultLabel.text { 
+            if isValidFormula(testStr: formule) {
+                resultLabel.text = calculateFormula(formula: formule)
+                return
+            }
+        }
+        displayErrorAlert()
     }
 
-    func displayAlert() {
+    let elements = ["0","1","2","3","4","5","6","7","8","9","+","*","/","-"]
+    @IBAction func tapButton(_ sender: UIButton) {
+      //  tag : 0, 1,2,3,4,5,6,7,8,9,10,11,12,13
+            addElementToFormula(element: elements[sender.tag])
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    func displayErrorAlert() {
         let alert = UIAlertController(title: "Error", message: "Enter a valid mathematical formula to calculate", preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "Dismiss", style: .default, handler: nil))
         self.present(alert, animated: true, completion: nil)
     }
 
-   func calculateFormula(formula : String) -> String{
-   
+    func calculateFormula(formula : String) -> String {
         let mathExpression = NSExpression(format: formula)
-    guard let mathValue = mathExpression.expressionValue(with: nil, context: nil) as? Int else {
-        fatalError(" !!! ")
-    }
+        guard let mathValue = mathExpression.expressionValue(with: nil, context: nil) as? Int else {
+        fatalError(" Error ")
+        }
             return String(mathValue)
     }
     
@@ -33,96 +53,14 @@ class ViewController: UIViewController {
         resultLabel.text!.append(element)
     }
     
-        @IBAction func resultButton(_ sender: UIButton) {
-    
-        if let formule = resultLabel.text {
-            if formule.isValidFormul{
-                resultLabel.text = calculateFormula(formula: formule)
-        }
-          else {displayAlert()}
-        }
-        else {
-            displayAlert()
-        }
-    }
-    
-    let elements = ["6","7","8","9","+","*","/","-","0","1","2","3","4","5"]
-    @IBAction func tap0(_ sender: UIButton) {
-        
-        let tag = sender.tag
-        switch tag {
-        case 0:
-            addElementToFormula(element: elements[8])
-         //   resultLabel.text! += "0"
-          
-        case 1:
-            addElementToFormula(element: elements[9])
-         //   resultLabel.text! += "1"
-           
-        case 2:
-            addElementToFormula(element: elements[10])
-          //  resultLabel.text! += "2"
-            
-        case 3:
-            addElementToFormula(element: elements[11])
-          //  resultLabel.text! += "3"
- 
-        case 4:
-           // resultLabel.text! += "4"
-            addElementToFormula(element: elements[12])
-          
-        case 5:
-           //resultLabel.text! += "5"
-            addElementToFormula(element: elements[13])
-            
-        case 6:
-            //  resultLabel.text! += "6"
-            addElementToFormula(element: elements[0])
-           
-        case 7:
-          //  resultLabel.text! += "7"
-            addElementToFormula(element: elements[1])
-            
-        case 8:
-            //  resultLabel.text! += "8"
-            addElementToFormula(element: elements[2])
-          
-        case 9:
-         //   resultLabel.text! += "9"
-            addElementToFormula(element: elements[3])
-           
-        case 10:
-         //   resultLabel.text! += "+"
-            addElementToFormula(element: elements[4])
-            
-        case 11:
-          //  resultLabel.text! += "-"
-            addElementToFormula(element: elements[7])
-          
-        case 12:
-            //   resultLabel.text! += "/"
-            addElementToFormula(element: elements[6])
-           
-        default:
-          //  resultLabel.text! += "*"
-            addElementToFormula(element: elements[5])
-           
-        }
-      
-    }
-    
-    
-    @IBAction func clearButton(_ sender: UIButton) {
-        resultLabel.text! = ""
-    }
-    
+    func isValidFormula(testStr:String) -> Bool {
+                let formuleRegEx = "[0-9]+([-+/*]?[0-9]+)*"
+                let formuleTest = NSPredicate(format:"SELF MATCHES %@", formuleRegEx)
+                let result = formuleTest.evaluate(with: testStr)
+                return result
+            }
 }
 
 
 ///https://stackoverflow.com/questions/29089741/swift-string-formula-into-a-real-calculation
 
-extension String {
-    var isValidFormul: Bool {
-        NSPredicate(format: "SELF MATCHES %@", "[[0-9]+[-*/+]?[0-9]+]+").evaluate(with: self)
-    }
-}
--- testTests/calculatingTests.swift
+++ testTests/calculatingTests.swift
@@ -13,25 +13,46 @@ class calculatingTests : XCTestCase {
 
     func testCalculateFormula() {
         let vc = ViewController()
+        //calculateFormula
         let add = vc.calculateFormula(formula: "5+5")
         XCTAssertEqual("10", add)
+        
         let minus = vc.calculateFormula(formula: "5-5")
         XCTAssertEqual("0", minus)
+        
         let divide = vc.calculateFormula(formula: "5/5")
         XCTAssertEqual("1", divide)
+        
         let multiply = vc.calculateFormula(formula: "5*5")
         XCTAssertEqual("25", multiply)
+        
         let multiplyAdd = vc.calculateFormula(formula: "5*5+1")
         XCTAssertEqual("26", multiplyAdd)
+        
         let divideMinus = vc.calculateFormula(formula: "30-5/5")
         XCTAssertEqual("29", divideMinus)
-        //how to test a void methode ?
-        /*func testAddElement(){
-            var text = "098"
-            let addElement = vc.addElementToFormula(element: "001")
-            
-        }*/
         
+        //validationFormula
+        let validationFormula = vc.isValidFormula(testStr: "5*5+1")
+        XCTAssertEqual(true, validationFormula)
+        
+        let validationFormula2 = vc.isValidFormula(testStr: "5*5+1+")
+        XCTAssertEqual(false, validationFormula2)
+        
+        let validationFormula3 = vc.isValidFormula(testStr: "+")
+        XCTAssertEqual(false, validationFormula3)
+        
+        let validationFormula4 = vc.isValidFormula(testStr: "*8")
+        XCTAssertEqual(false, validationFormula4)
+        
+        let validationFormula5 = vc.isValidFormula(testStr: "987++")
+        XCTAssertEqual(false, validationFormula5)
+        
+        let validationFormula6 = vc.isValidFormula(testStr: "")
+        XCTAssertEqual(false, validationFormula6)
+        
+        let validationFormula7 = vc.isValidFormula(testStr: "4+-9")
+        XCTAssertEqual(false, validationFormula7)
     }
     
 }
